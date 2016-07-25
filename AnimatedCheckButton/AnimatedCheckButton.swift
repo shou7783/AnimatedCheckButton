b8d16ba85d1b5a9a8832caab91a2d10beb84a039
MengHsiu Chang <shou@MengHsius-MacBook-Pro.local>
2016-07-25 12:29:04 +0800

    add license

--- AnimatedCheckButton/AnimatedCheckButton.swift
+++ AnimatedCheckButton/AnimatedCheckButton.swift
@@ -2,8 +2,27 @@
 //  AnimatedCheckButton.swift
 //  AnimatedCheckButton
 //
-//  Created by MengHsiu Chang on 7/22/16.
-//  Copyright © 2016 shou. All rights reserved.
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2016 MengHsiu Chang
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import UIKit
@@ -137,14 +156,6 @@ class AnimatedCheckButton: UIButton {
         circleLayer.path = UIBezierPath(ovalInRect: CGRect(origin: CGPointZero, size: self.circleSize)).CGPath
         animatedLayer.path = self.getAnimatedPath().CGPath
     }
-    
-//    override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
-//        let size = self.circleSize
-//        if (point.x < size.width && point.y < size.height) {
-//            return self;
-//        }
-//        return nil;
-//    }
     
     override func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool {
         let size = self.circleSize
--- AnimatedCheckButton/ViewController.swift
+++ AnimatedCheckButton/ViewController.swift
@@ -15,13 +15,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         let button = AnimatedCheckButton(frame: CGRectMake(100, 50, 200, 50))
-        button.lineWidth = 50
+        button.lineWidth = 5
+        button.circleAlpha = 0
         self.view.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Top, relatedBy: .Equal, toItem: button, attribute: .Top, multiplier: 1, constant: -50))
-        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Bottom, relatedBy: .Equal, toItem: button, attribute: .Bottom, multiplier: 1, constant: 0))
-        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Leading, relatedBy: .Equal, toItem: button, attribute: .Leading, multiplier: 1, constant: 0))
-        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Trailing, relatedBy: .Equal, toItem: button, attribute: .Trailing, multiplier: 1, constant: 0))
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Top, relatedBy: .Equal, toItem: button, attribute: .Top, multiplier: 1, constant: -50))
+//        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Bottom, relatedBy: .Equal, toItem: button, attribute: .Bottom, multiplier: 1, constant: 0))
+//        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Leading, relatedBy: .Equal, toItem: button, attribute: .Leading, multiplier: 1, constant: 0))
+//        self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .Trailing, relatedBy: .Equal, toItem: button, attribute: .Trailing, multiplier: 1, constant: 0))
     }
 
     override func didReceiveMemoryWarning() {
